c76d0384dac6eed27cea28c2fbc44178ca995330
PodverbnyAA <90952984+RedPhantom36@users.noreply.github.com>
2021-09-18 17:21:51 +0700

    Add files via upload

---
+++ second_project.cs
@@ -0,0 +1,20 @@
+<<<<<<< HEAD
+First Line
+
+Second Line
+
+OMG This Is  The Third Mistake
+=======
+Second Line
+
+First Line
+
+What?
+<<<<<<< HEAD
+>>>>>>> the_first_branch
+
+And Finally.. The Fourth Mistake!
+=======
+
+Not.. Really?
+>>>>>>> the_first_branch
